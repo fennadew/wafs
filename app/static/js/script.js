a9be9a7ce6c45b6afb98d29d77ae1658c26f58e3
Fenna <fennadewilde@hotmail.com>
2018-02-16 15:44:14 +0100

    added a promise

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -11,61 +11,69 @@
     // API to request data from meme generator
     const api = {
         getData(name) {
-            const request = new XMLHttpRequest();
-            request.open('GET', 'http://version1.api.memegenerator.net//Generators_Select_ByPopular?pageIndex=0&pageSize=12&days=&apiKey=893dfe6c-e0e7-4693-8a9e-5df5de998357', true);
-            request.onload = () => {
-                if (request.status >= 200 && request.status < 400) {
-                    // Succeses!
-                    const data = JSON.parse(request.responseText)
+            const loadData = new Promise(function(resolve, reject) {
+                const request = new XMLHttpRequest();
+                request.open('GET', 'http://version1.api.memegenerator.net//Generators_Select_ByPopular?pageIndex=0&pageSize=12&days=&apiKey=893dfe6c-e0e7-4693-8a9e-5df5de998357', true);
 
-                    const memeData = data.result.map((obj) => {
-                        return {
-                            displayName: obj.displayName,
-                            imageUrl: obj.imageUrl,
-                            href: obj.displayName.toLowerCase().replace(/ $/, "").split(' ').join('-')
-                        }
-                    })
-
-                    /* if you are on a meme page, the name of the meme is passed as a parameter.
-                    The if statement checks if you are on a meme page or on the overview page
-                    */
-
-                    if (name) {
-                        const selectedMeme = memeData.filter((obj) => {
-                            return obj.href === name;
-                        })
-                        const directives = {
-                            image: {
-                                src(params) {
-                                    return this.imageUrl
-                                }
-                            }
-                        }
-                        Transparency.render(document.getElementById('details'), selectedMeme, directives);
-
+                request.onload = () => {
+                    if (request.status >= 200 && request.status < 400) {
+                        // Success!
+                        const data = JSON.parse(request.responseText);
+                        resolve(data);
+                    } else {
+                        // We reached our target server, but it returned an error
+                        reject(request.error);
                     }
-                    else {
-                        const directives = {
-                            displayName: {
-                                // Some names have an space at the end. The replace removes that space so the won't be an extra - at the end of the string when the spaces are replaced with -
-                                href(params) {
-                                    return "#populair/" + this.displayName.toLowerCase().replace(/ $/, "").split(' ').join('-');
-                                }
-                            }
-                        }
-
-                        Transparency.render(document.getElementById('memes'), memeData, directives);
-                    }
-
-
-                } else {
-// We reached our target server, but it returned an error
                 }
-            }
-            request.onerror = function () {
-// There was a connection error of some sort
-            }
-            request.send();
+                request.onerror = () => {
+                    // There was a connection error of some sort
+                    console.log("failed to request")
+                };
+
+                request.send();
+            })
+
+
+            loadData.then(data => {
+                //render first three items
+                const memeData = data.result.map((obj) => {
+                    return {
+                        displayName: obj.displayName,
+                        imageUrl: obj.imageUrl,
+                        href: obj.displayName.toLowerCase().replace(/ $/, "").split(' ').join('-')
+                    }
+                })
+
+                if (name) {
+                    const selectedMeme = memeData.filter((obj) => {
+                        return obj.href === name;
+                    })
+                    const directives = {
+                        image: {
+                            src(params) {
+                                return this.imageUrl
+                            }
+                        }
+                    }
+                    Transparency.render(document.getElementById('details'), selectedMeme, directives);
+
+                }
+                else {
+                    const directives = {
+                        displayName: {
+                            // Some names have an space at the end. The replace removes that space so the won't be an extra - at the end of the string when the spaces are replaced with -
+                            href(params) {
+                                return "#populair/" + this.displayName.toLowerCase().replace(/ $/, "").split(' ').join('-');
+                            }
+                        }
+                    }
+
+                    Transparency.render(document.getElementById('memes'), memeData, directives);
+                }
+
+            }).catch(error => {
+                console.log(error)
+            })
         }
     }
 
